872f0538018ad314f4cb318327753e1267d50cea
SALAS DRAI <ismael.montoya@udea.edu.co>
2025-07-24 17:02:07 -0500

    esta cosa tiene un erro que no conozco

--- UDEA_TT_JuegoBingoJS-main/codigo/cantor.js
+++ UDEA_TT_JuegoBingoJS-main/codigo/cantor.js
@@ -1,5 +1,7 @@
 function Cantor() {
 
+    this.balotasSacadas = new Array(75).fill(false);
+    this.ultimaBalota = 0
     this.tabla = [];
     for (var f = 0; f < 15; f++) {
         this.tabla[f] = [];
@@ -8,4 +10,20 @@ function Cantor() {
         }
 
     }
+    //metodo para iniciar
+    this.iniciar = function () {
+        this.balotasSacadas = new Array(75).fill(false);
+    }
+
+
+    //metodo para sacra balotas
+    this.sacarBalota = function () {
+        let balota = Math.floor(Math.random() * 75) + 1;
+        this.balotasSacadas[balota - 1] = true;
+        this.ultimaBalota = balota;
+
+        window.alert("esta vez si funciona")
+    }
 }
+
+
